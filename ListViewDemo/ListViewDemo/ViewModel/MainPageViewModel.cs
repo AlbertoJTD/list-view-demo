e70682ccbf55c07724b9329c3ccd084f980e678b
AlbertoJTD <albertojtd@outlook.com>
2023-07-05 09:17:08 -0600

    Add MainPageViewModel

---
+++ ListViewDemo/ListViewDemo/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ListViewDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListViewDemo.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public List<Friend> Friends { get; set; }
+        public MainPageViewModel()
+        {
+            FriendRepository friendRepository = new FriendRepository();
+            Friends = friendRepository.GetAllFriends().ToList();
+        }
+    }
+}
