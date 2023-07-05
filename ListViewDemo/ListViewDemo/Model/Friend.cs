f2c49ea95a5b73a9af417bb91c1985ec81f56f04
AlbertoJTD <albertojtd@outlook.com>
2023-07-05 09:07:44 -0600

    Add Friend model

---
+++ ListViewDemo/ListViewDemo/Model/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewDemo.Model
+{
+    public class Friend
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ ListViewDemo/ListViewDemo/Model/FriendRepository.cs
@@ -0,0 +1,24 @@
+﻿using Foundation.ObjectHydrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewDemo.Model
+{
+    public class FriendRepository
+    {
+        public IList<Friend> Friends { get; set; }
+
+        public FriendRepository()
+        {
+            // Populate a list based on Friend model attributes
+            Hydrator<Friend> _friendHydrator = new Hydrator<Friend>();
+            Friends = _friendHydrator.GetList(50);
+        }
+
+        public IList<Friend> GetAllFriends()
+        {
+            return Friends;
+        }
+    }
+}
