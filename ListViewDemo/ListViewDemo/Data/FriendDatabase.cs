1692a80e56d5ccb52ea055acc7b4c1bd62ad6e7c
AlbertoJTD <albertojtd@outlook.com>
2023-07-09 07:41:32 -0600

    Add FriendDatabase

---
+++ ListViewDemo/ListViewDemo/Data/FriendDatabase.cs
@@ -0,0 +1,19 @@
+﻿using ListViewDemo.Model;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewDemo.Data
+{
+	public class FriendDatabase
+	{
+		readonly SQLiteAsyncConnection database;
+
+        public FriendDatabase(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<Friend>().Wait();
+        }
+    }
+}
