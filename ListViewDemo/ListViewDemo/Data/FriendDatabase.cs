095dd9f773bc1117ea9693ceece28eec898e8b40
AlbertoJTD <albertojtd@outlook.com>
2023-07-09 07:59:16 -0600

    Add CRUD methods

--- ListViewDemo/ListViewDemo/Data/FriendDatabase.cs
+++ ListViewDemo/ListViewDemo/Data/FriendDatabase.cs
@@ -3,6 +3,7 @@ using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ListViewDemo.Data
 {
@@ -14,6 +15,33 @@ namespace ListViewDemo.Data
         {
             database = new SQLiteAsyncConnection(dbPath);
             database.CreateTableAsync<Friend>().Wait();
+        }
+
+        public async Task<List<Friend>> GetItemsAsync()
+        {
+            return await database.Table<Friend>().ToListAsync();
+        }
+
+        public Task<Friend> GetItemAsync(int id)
+        {
+            return database.Table<Friend>().Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveItemAsync(Friend item)
+        {
+            if (item.Id != 0)
+            {
+                return database.UpdateAsync(item);
+            }
+            else
+            {
+                return database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(Friend item)
+        {
+            return database.DeleteAsync(item);
         }
     }
 }
