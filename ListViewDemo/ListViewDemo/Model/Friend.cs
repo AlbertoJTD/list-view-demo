6811e44f97d3694569fb5cc396d58bcdb6f44545
AlbertoJTD <albertojtd@outlook.com>
2023-07-10 07:57:57 -0600

    Add FriendViewViewModel & INotifyPropertyChanged implementation

--- ListViewDemo/ListViewDemo/Model/Friend.cs
+++ ListViewDemo/ListViewDemo/Model/Friend.cs
@@ -1,19 +1,56 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace ListViewDemo.Model
 {
-    public class Friend
+    public class Friend: INotifyPropertyChanged
     {
-        [PrimaryKey, AutoIncrement]
+		private string firstName;
+		private string phone;
+		private string email;
+
+		[PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
-        public string FirstName { get; set; }
+		public string FirstName
+		{
+			get => firstName;
+			set
+			{
+				firstName = value;
+				OnPropertyChanged();
+			}
+		}
 
-        public string Phone { get; set; }
+		public string Phone
+		{
+			get => phone;
+			set
+			{
+				phone = value;
+				OnPropertyChanged();
+			}
+		}
 
-        public string Email { get; set; }
-    }
+		public string Email
+		{
+			get => email;
+			set
+			{
+				email = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs((propertyName)));
+		}
+	}
 }
