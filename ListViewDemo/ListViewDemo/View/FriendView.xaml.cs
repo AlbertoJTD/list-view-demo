942fba32ca9b015b9646ebbe9957dac51426c898
AlbertoJTD <albertojtd@outlook.com>
2023-07-10 07:39:55 -0600

    Add FriendView

---
+++ ListViewDemo/ListViewDemo/View/FriendView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewDemo.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FriendView : ContentPage
+	{
+		public FriendView()
+		{
+			InitializeComponent();
+		}
+	}
+}
